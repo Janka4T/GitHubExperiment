960dc668abb9bd099360905e10e9eaecbcb6f9b3
Jānis Tulišs <janistuliss3@gmail.com>
2020-04-03 17:15:38 +0300

    v.5.15

--- GitHubExperiment/ButtonGame.cs
+++ GitHubExperiment/ButtonGame.cs
@@ -16,5 +16,16 @@ namespace GitHubExperiment
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ShowMessage(button1.Text);
+        }   
+     public void ShowMessage(string message)
+        {
+        MessageBox.Show(message);
+        }    
     }
 }
+
+       
