903959a0c0b60fb8cd5d6aa8d38043776c788aca
Jānis Tulišs <janistuliss3@gmail.com>
2020-04-03 17:05:00 +0300

    v5.04 PM

--- GitHubExperiment/ButtonGame.Designer.cs
+++ GitHubExperiment/ButtonGame.Designer.cs
@@ -30,11 +30,12 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(23, 41);
+            this.button1.Location = new System.Drawing.Point(36, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(328, 176);
             this.button1.TabIndex = 0;
@@ -43,21 +44,31 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(387, 41);
+            this.button2.Location = new System.Drawing.Point(418, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(328, 176);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(36, 203);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(328, 176);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // ButtonGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.Name = "ButtonGame";
             this.Text = "Form1";
             this.ResumeLayout(false);
 
@@ -67,6 +78,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
